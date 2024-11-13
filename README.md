fdac053227514c7d06941fb95764c9d3fdb7ae66
Riders004 <Riders004@mail.googlecom>
2024-11-13 11:05:09 +0530

    Intial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# FreeRDP
+
+
+- Free Windows RDP 6 Hours
+
+- Create RDP Windows 10 Ram 7GB 2 Core Cpu With Github:
+
+- Press the Fork Button to make RDP (For Android/HP Users Please Use Desktop Mode).
+
+- visit https://dashboard.ngrok.com to get NGROK_AUTH_TOKEN
+
+- Inside this Repo Go to Settings > Secrets > New repository secret
+
+- Fill Name: Enter NGROK_AUTH_TOKEN
+
+- Fill Value: Visit https://dashboard.ngrok.com/auth/your-authtoken Copy and Paste in the value
+
+- Press Add secret
+
+- Go to Action <Click Select workflow< Select Har Pito, Press Run workflow
+
+- Refresh the Web/page and go to Har Pito > click build
+
+- Wait 1-5 minutes
+
+- Press the down arrow button that says (Connect RDP.) to get IP, User, Password.
+
+
+Warning if Rdp close please click Action<Select Har Pito<Click build, Click on the three dots click run-rensjob and run the Rdp again
---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By mohammadali" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user mohammadali mmd@123 /add >nul
+net localgroup administrators mohammadali /add >nul
+net user mohammadali /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant mohammadali:F >nul
+ICACLS C:\Windows\installer /grant mohammadali:F >nul
+echo Successfully installed! If RDP is dead, rebuild again.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: mohammadali
+echo Password: mmd@123
+echo You can login now
+ping -n 10 127.0.0.1 >nul
