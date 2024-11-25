2cd3aef5b1247beb491778dcb06f53889f3bd41b
zwlucas <160072085+zwlucas@users.noreply.github.com>
2024-11-25 17:13:49 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,7 +14,6 @@ ICACLS C:\Windows\Temp /grant administrator :F >nul
 ICACLS C:\Windows\installer /grant administrator :F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
-tasklist | find /i "ngrok/ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator 
 echo Password: OLDUSER#6
 echo You can login now
